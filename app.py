bbd72c5b7847fece6ddef0edcf6f94726cbe4cd2
ChristianLopez666 <directorio.profesion@gmail.com>
2025-11-04 16:23:24 -0700

    Update fmt.Println output from 'Hello World'

--- app.py
+++ app.py
@@ -1194,10 +1194,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     logger.info("Iniciando Flask en puerto %d", port)
     app.run(host="0.0.0.0", port=port, debug=False)
-
-
-
-
-
-
-
